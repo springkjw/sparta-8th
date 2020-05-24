68ea1ddc90d604ecd2bb8b609cfd7e542b440d24
JaewonKwon <springkjw@gmail.com>
2020-05-24 16:26:48 +0900

    3week

--- 3week/openapi.py
+++ 3week/openapi.py
@@ -19,4 +19,12 @@ import requests # requests 라이브러리 설치 필요
 
 r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
 rjson = r.json() # Json 데이터를 자동으로 Python Dictionary로 변환
-print(rjson['RealtimeCityAir']['row'][0]['NO2'])
+
+realtime_city_air = rjson["RealtimeCityAir"]
+rows = realtime_city_air["row"] # 리스트
+
+for row in rows:
+    gu_name = row["MSRSTE_NM"]
+    mise_value = row["IDEX_MVL"]
+
+    print(gu_name, mise_value)
