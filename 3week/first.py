13853b0993ed2f4b6246fd665c60e3c3662769a8
JaewonKwon <springkjw@gmail.com>
2020-05-24 14:15:51 +0900

    3week

---
+++ 3week/first.py
@@ -0,0 +1,21 @@
+variable = 1
+test = 'sparta'
+
+# 자바스크립트 변수명: Camel:  spartaCodingClub
+# 파이썬 변수명: Snake:      sparta_coding_club
+
+a = "sparta"
+a1 = 'sparta'
+# a2 = `` X
+# let html = `<li></li>`
+a2 = """
+Sparta Coding Club
+"""
+# 따옴표 3개 주석 처리 할 때 또는 함수를 설명할 때(문서화)
+
+a = "Hello"
+b = "world"
+a + b # Helloword
+
+a = "<li>{}</li>{}".format(test, test2)
+a1 = "<li>{test}</li>{test1}".format(test=test, test1=test1)
