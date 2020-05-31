d6bcd696071385e99153332041b87fcf90c84254
Ellie <chltjgus814@gmail.com>
2020-05-31 13:17:17 +0900

    4week

--- 4week/homework/genie_music.py
+++ 4week/homework/genie_music.py
@@ -11,7 +11,14 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 # requests 로 HTML 파일을 불러오기
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 }
+data = requests.get(
+    'https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',
+    headers=headers
+)
 
+soup = BeautifulSoup(data.text, 'html.parser')
+print(soup)
