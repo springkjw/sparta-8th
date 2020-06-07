52f9cb6334b5e4dacfb8cf2dd5b1a2dbe2bc337b
권재원 <springkjw@gmail.com>
2020-06-07 14:30:26 +0900

    5week

--- 5week/moviestar/app.py
+++ 5week/moviestar/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
 client = MongoClient('localhost', port=27017)
@@ -10,6 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/api/list')
+def star_retreive():
+    stars = list(db.mystar.find({}, {'_id': False}))
+    return jsonify({
+        'result': 'success',
+        'stars': stars
+    })
 
 
 if __name__ == '__main__':
